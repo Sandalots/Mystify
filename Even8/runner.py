8913b45227a904e5a7d867f2e93a5f4b25f3dab9
Sandy MacDonald <59518103+Sandalots@users.noreply.github.com>
2023-05-20 02:03:49 +0100

    updated runner

--- Even8/runner.py
+++ Even8/runner.py
@@ -1,5 +1,20 @@
 # run the program
 import os
 
+# check if config.yaml exists
+if not os.path.exists("config.yaml"):
+    print("config.yaml not found")
+    quit()
+    
+# check if output.csv exists
+if os.path.exists("output.csv"):
+    # delete output.csv
+    os.remove("output.csv")
+
+# check if Even8/main.py exists
+if not os.path.exists("Even8/main.py"):
+    print("Even8/main.py not found")
+    quit()
+    
 os.system("python3 Even8/main.py gateway")
 quit()
